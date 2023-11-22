8a5aa10fbe9d0812a2baa9185189c19c6d1ff189
ChrisCoy <christopherleegs@outlook.com>
2023-11-21 22:27:41 -0300

    improve problem6 readability

--- lib/solutions/solution6.ex
+++ lib/solutions/solution6.ex
@@ -1,6 +1,6 @@
 # 6. Find out whether a list is a palindrome. (easy)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution6 do
-  alias NinetyNineElixirProblems.Solutions.Solution5
-  def call(list), do: Solution5.call(list) == list
+  alias NinetyNineElixirProblems.Solutions.Solution5, as: Reverse
+  def call(list), do: Reverse.call(list) == list
 end
