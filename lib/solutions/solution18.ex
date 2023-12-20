3d2deba2029ab0c52c6cea3a75d8d023647aa46a
ChrisCoy <christopherleegs@outlook.com>
2023-12-20 17:03:23 -0300

    change functions to private

--- lib/solutions/solution18.ex
+++ lib/solutions/solution18.ex
@@ -7,10 +7,10 @@
 defmodule NinetyNineElixirProblems.Solutions.Solution18 do
   def call(list, start, ending), do: till_start_zero(list, start, ending)
 
-  def till_start_zero(list, 0, ending), do: till_ending_zero(list, ending)
-  def till_start_zero([_h | t], start, ending), do: till_start_zero(t, start - 1, ending)
+  defp till_start_zero(list, 0, ending), do: till_ending_zero(list, ending)
+  defp till_start_zero([_h | t], start, ending), do: till_start_zero(t, start - 1, ending)
 
-  def till_ending_zero(a, b, c \\ [])
-  def till_ending_zero(_list, 1, acc), do: acc
-  def till_ending_zero([h | t], ending, acc), do: till_ending_zero(t, ending - 1, acc ++ [h])
+  defp till_ending_zero(a, b, c \\ [])
+  defp till_ending_zero(_list, 1, acc), do: acc
+  defp till_ending_zero([h | t], ending, acc), do: till_ending_zero(t, ending - 1, acc ++ [h])
 end
