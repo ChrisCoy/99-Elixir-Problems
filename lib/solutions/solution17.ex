02c25c49960f1b4ef893958bed410f86e3fd3f46
ChrisCoy <christopherleegs@outlook.com>
2023-12-20 15:33:57 -0300

    fix: remove unused function

--- lib/solutions/solution17.ex
+++ lib/solutions/solution17.ex
@@ -1,8 +1,6 @@
 # 17. Split a list into two parts; the length of the first part is given. (easy)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution17 do
-  def call([], _nth, _cont, list), do: list
-
   def call([], _length, acc \\ []), do: [acc, []]
   def call(list, 0, acc), do: [acc, list]
   def call([h | t], length, acc), do: call(t, length - 1, acc ++ [h])
