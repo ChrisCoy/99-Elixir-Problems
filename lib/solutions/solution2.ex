a5fd0119fbe87c2b691cd285d13af61e6f7a8db2
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:14:14 -0300

    improve solution2 readability

--- lib/solutions/solution2.ex
+++ lib/solutions/solution2.ex
@@ -2,7 +2,7 @@
 # 2. Find the last but one (last and penultimate) elements of a list. (easy)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution2 do
-  def call([]), do: nil
-  def call([first, second]), do: [first, second]
-  def call([_head | tail]), do: call(tail)
+  def last_two([]), do: nil
+  def last_two([first, second]), do: [first, second]
+  def last_two([_head | tail]), do: last_two(tail)
 end
