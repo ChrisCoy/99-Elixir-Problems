04d47ec305db003973f282ba2b0e9fb080154c8f
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:13:54 -0300

    improve solution1 readability

--- lib/solutions/solution1.ex
+++ lib/solutions/solution1.ex
@@ -2,7 +2,7 @@
 # 1. Write a function last : 'a list -> 'a option that returns the last element of a list. (easy)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution1 do
-  def call([]), do: nil
-  def call([el]), do: el
-  def call([_head | tail]), do: call(tail)
+  def last([]), do: nil
+  def last([el]), do: el
+  def last([_head | tail]), do: last(tail)
 end
