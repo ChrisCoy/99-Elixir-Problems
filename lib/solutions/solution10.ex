8d4bfc0f376c630ab26fad00b51614fe4e3573a6
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:38:09 -0300

    improve solution10 readability

--- lib/solutions/solution10.ex
+++ lib/solutions/solution10.ex
@@ -1,10 +1,11 @@
 # 10. Run-length encoding of a list. (easy)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution10 do
-  def call([], map), do: Map.to_list(map)
+  def encode(list, map \\ %{})
+  def encode([], map), do: Map.to_list(map)
 
-  def call([head | tail], map \\ %{}) do
+  def encode([head | tail], map) do
     new_map = Map.update(map, head, 1, fn n -> n + 1 end)
-    call(tail, new_map)
+    encode(tail, new_map)
   end
 end
