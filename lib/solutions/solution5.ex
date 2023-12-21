fa7bb23a38c37eaea547df5ba4e0f4b90807f6da
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:14:38 -0300

    improve solution5 readability

--- lib/solutions/solution5.ex
+++ lib/solutions/solution5.ex
@@ -1,6 +1,7 @@
 # 5. Reverse a list. (easy)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution5 do
-  def call([], reverse), do: reverse
-  def call([head | tail], reverse \\ []), do: call(tail, [head] ++ reverse)
+  def rev(list, reverse \\ [])
+  def rev([], reverse), do: reverse
+  def rev([head | tail], reverse), do: rev(tail, [head] ++ reverse)
 end
