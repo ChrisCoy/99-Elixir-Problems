2c76ab7426c78c46f7d6a4adbae4ab95a128bdf5
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:39:30 -0300

    improve solution23 readability

--- lib/solutions/solution23.ex
+++ lib/solutions/solution23.ex
@@ -6,21 +6,18 @@ defmodule NinetyNineElixirProblems.Solutions.Solution23 do
   alias NinetyNineElixirProblems.Solutions.Solution4, as: ListSize
   alias :rand, as: Rand
 
-  def call(list, amount, acc \\ [])
-  def call(list, amount, _acc) when amount >= length(list), do: list
-  def call([], _amount, acc), do: acc
-  def call(_list, 0, acc), do: acc
+  def rand_select(list, amount, acc \\ [])
+  def rand_select(list, amount, _acc) when amount >= length(list), do: list
+  def rand_select([], _amount, acc), do: acc
+  def rand_select(_list, 0, acc), do: acc
 
-  def call(list, amount, acc) do
-    {picked_element, rest_list} = pick_at(list, Rand.uniform(ListSize.call(list) - 1))
+  def rand_select(list, amount, acc) do
+    {picked_element, rest_list} = pick_at(list, Rand.uniform(ListSize.length(list) - 1))
 
-    call(rest_list, amount - 1, [picked_element | acc])
+    rand_select(rest_list, amount - 1, [picked_element | acc])
   end
 
   defp pick_at(list, index, acc \\ [])
   defp pick_at([h | t], 0, acc), do: {h, t ++ acc}
   defp pick_at([h | t], index, acc), do: pick_at(t, index - 1, [h | acc])
-
-  # Math.
-  # def call(from, to), do: range(from, to, [], get_signal(from, to))
 end
