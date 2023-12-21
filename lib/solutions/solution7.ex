a0a614d8c468abcf3d0404249a05049f3d474c39
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:14:53 -0300

    improve solution7 readability

--- lib/solutions/solution7.ex
+++ lib/solutions/solution7.ex
@@ -1,14 +1,15 @@
 # 7. Flatten a nested list structure. (medium)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution7 do
-  def call([], acc), do: acc
+  def flatten(list, acc \\ [])
+  def flatten([], acc), do: acc
 
-  def call(list, acc \\ []) do
+  def flatten(list, acc) do
     [head | tail] = list
 
     case is_list(head) do
-      true -> call(tail, acc ++ head)
-      false -> call(tail, acc ++ [head])
+      true -> flatten(tail, acc ++ head)
+      false -> flatten(tail, acc ++ [head])
     end
   end
 end
