f44c155005d2b233cce60eebd0a89d9fd974349c
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:39:24 -0300

    improve solution22 readability

--- lib/solutions/solution22.ex
+++ lib/solutions/solution22.ex
@@ -3,14 +3,12 @@
 # If first argument is greater than second, produce a list in decreasing order.
 
 defmodule NinetyNineElixirProblems.Solutions.Solution22 do
-  def call(from, to), do: range(from, to, [], get_sign(from, to))
+  def range(from, to), do: create_range(from, to, [], get_sign(from, to))
 
-  defp range(from, from, acc, _sign), do: acc ++ [from]
-  defp range(from, to, acc, sign), do: range(from + sign, to, acc ++ [from], sign)
+  defp create_range(from, from, acc, _sign), do: acc ++ [from]
+  defp create_range(from, to, acc, sign), do: create_range(from + sign, to, acc ++ [from], sign)
 
   defp get_sign(from, to), do: if(from < to, do: 1, else: -1)
-
-
 
   ############## SLOW IMPLEMENTATION ##############
   # it is slow because it have to calculate the sign every time it calls
