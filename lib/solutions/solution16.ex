ed92a9d8ef1498e24c716d7cd7418f6652ec7ff3
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:38:41 -0300

    improve solution16 readability

--- lib/solutions/solution16.ex
+++ lib/solutions/solution16.ex
@@ -1,13 +1,14 @@
 # 16. Drop every N'th element from a list. (medium)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution16 do
-  def call([], _nth, _cont, list), do: list
+  def drop(list, nth, cont \\ 1, list \\ [])
+  def drop([], _nth, _cont, list), do: list
 
-  def call([head | tail], nth, cont \\ 1, list \\ []) do
+  def drop([head | tail], nth, cont, list) do
     if rem(cont, nth) == 0 do
-      call(tail, nth, cont + 1, list)
+      drop(tail, nth, cont + 1, list)
     else
-      call(tail, nth, cont + 1, list ++ [head])
+      drop(tail, nth, cont + 1, list ++ [head])
     end
   end
 end
