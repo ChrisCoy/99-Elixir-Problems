6dd82891f6c7109683a321241f4061699f249f94
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:38:26 -0300

    improve solution14 readability

--- lib/solutions/solution14.ex
+++ lib/solutions/solution14.ex
@@ -1,6 +1,7 @@
 # 14. Duplicate the elements of a list. (easy)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution14 do
-  def call([head | tail], acc \\ []), do: call(tail, acc ++ [head, head])
-  def call([], acc), do: acc
+  def duplicate(list, acc \\ [])
+  def duplicate([], acc), do: acc
+  def duplicate([head | tail], acc), do: duplicate(tail, acc ++ [head, head])
 end
