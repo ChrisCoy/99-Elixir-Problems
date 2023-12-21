e6abba41b23b08dc0ad6b61376e1457abb34d6a8
ChrisCoy <christopherleegs@outlook.com>
2023-12-21 10:14:21 -0300

    improve solution3 readability

--- lib/solutions/solution3.ex
+++ lib/solutions/solution3.ex
@@ -1,8 +1,8 @@
 # 3. Find the K'th element of a list. (easy)
 
 defmodule NinetyNineElixirProblems.Solutions.Solution3 do
-  def call(_list, 0), do: nil
-  def call([], _nth), do: nil
-  def call([head | _tail], 1), do: head
-  def call([_head | tail], nth), do: call(tail, nth - 1)
+  def at(_list, 0), do: nil
+  def at([], _nth), do: nil
+  def at([head | _tail], 1), do: head
+  def at([_head | tail], nth), do: at(tail, nth - 1)
 end
