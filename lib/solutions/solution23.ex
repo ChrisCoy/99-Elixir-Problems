a4f200bc7ed522999679512ffc0c157b6687ad46
ChrisCoy <christopherleegs@outlook.com>
2023-12-22 10:27:24 -0300

    fix solution 23

--- lib/solutions/solution23.ex
+++ lib/solutions/solution23.ex
@@ -7,17 +7,18 @@ defmodule NinetyNineElixirProblems.Solutions.Solution23 do
   alias :rand, as: Rand
 
   def rand_select(list, amount, acc \\ [])
-  def rand_select(list, amount, _acc) when amount >= length(list), do: list
   def rand_select([], _amount, acc), do: acc
   def rand_select(_list, 0, acc), do: acc
 
   def rand_select(list, amount, acc) do
-    {picked_element, rest_list} = pick_at(list, Rand.uniform(ListSize.length(list) - 1))
+    random_index = Rand.uniform(max(1, ListSize.length(list) - 1))
+    {picked_element, rest_list} = pick_at(list, random_index)
 
     rand_select(rest_list, amount - 1, [picked_element | acc])
   end
 
   defp pick_at(list, index, acc \\ [])
+  defp pick_at([], _index, [el]), do: {el, []}
   defp pick_at([h | t], 0, acc), do: {h, t ++ acc}
   defp pick_at([h | t], index, acc), do: pick_at(t, index - 1, [h | acc])
 end
